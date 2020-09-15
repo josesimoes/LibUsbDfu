5d88477452b5d78edca6bbbe7adb506fd5ad58fa
josesimoes <jose.simoes@eclo.solutions>
2020-09-15 09:09:34 +0100

    Update Solution and project
    - Migrate to .NET 4.7.1
    - Other tweaks

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
+﻿using DeviceProgramming.FileFormat;
 using DeviceProgramming.Memory;
-using DeviceProgramming.FileFormat;
 using LibUsbDotNet;
 using Mono.Options;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace LibUsbDfu
 {
